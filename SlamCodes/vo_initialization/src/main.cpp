b8fecdad2b9fa5b617ed4432a6c3da2657399077
kongdaqing <kongdaqing@zerozero.cn>
2020-09-08 21:10:54 +0800

    update vo_initialization

--- SlamCodes/vo_initialization/src/main.cpp
+++ SlamCodes/vo_initialization/src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#include <opencv/cv.h>
+#include <opencv2/viz.hpp>
+#include <eigen3/Eigen/Dense>
 int main(int argc,char** argv) {
+  cv::Mat a;
+  Eigen::Vector2f ad;
   printf("Hello VO!\n");
 }
