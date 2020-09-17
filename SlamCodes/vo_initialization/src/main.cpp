8152b8c056226feade3fc30b41226da6eb37aa4c
kongdaqing <kongdaqing@zerozero.cn>
2020-09-17 21:02:15 +0800

    add viz_scene lib

--- SlamCodes/vo_initialization/src/main.cpp
+++ SlamCodes/vo_initialization/src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <opencv2/viz.hpp>
 #include <eigen3/Eigen/Dense>
+#include "viz_scene/viz_scene.hpp"
+using namespace viz_scene;
 int main(int argc,char** argv) {
   cv::Mat a;
+  VizScene testViz("windows");
   Eigen::Vector2f ad;
+  while(1) {
+    usleep(10);
+  }
   printf("Hello VO!\n");
 }
