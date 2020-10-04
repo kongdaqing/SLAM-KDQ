bc99f66211efe52c9fd09bd6354b4833bd2db34d
kongdaqing <kongdaqing@zerozero.cn>
2020-10-04 20:32:39 +0800

    create total header for install

--- SlamCodes/feature_track/inc/feature_track/optiflow_track/vins_optitrack.hpp
+++ SlamCodes/feature_track/inc/feature_track/optiflow_track/vins_optitrack.hpp
@@ -14,19 +14,41 @@ class OptiflowTrackByVINS {
     bool trackBack_;
     float min_dist_;
     std::string save_image_path_;
-    void addNewPoints();
+    /** @brief add mask at points those were tracked  
+      * @param none
+      */ 
     void setMask();
-    void showTrack();
-    double distance(const cv::Point2f& pt1,const cv::Point2f& pt2);
-    bool inBorder(const cv::Point2f& pt);
+    /** @brief show track result 
+      * @param none 
+      */ 
     void drawTrack();
+    /** @brief  calculate distance of pixel range between two pixels
+      * @param pt1 - pixel coordinate in image
+      * @param pt2 - pixel coordinate in another image
+      */ 
+    double distance(const cv::Point2f& pt1,const cv::Point2f& pt2);
+    /** @brief check pixel coordinate whether is out of image size or not 
+      * @param pt - pixel coordinate
+      */  
+    bool inBorder(const cv::Point2f& pt);
+    /** @brief reduce object vector throngh the status vector 
+      * @param status - status vector
+      * @param vec - object vector 
+      */  
     template <typename RType>
     static void reduceVector(const std::vector<uchar>& status,std::vector<RType>& vec);
 
   public:
+    /** @brief Constructor
+      */ 
     OptiflowTrackByVINS(int featSize,float thres,float minDist,std::string savePath,bool trackBack);
+    /** @brief Disconstructor
+      */ 
     ~OptiflowTrackByVINS();
-
+    /** @brief track features in current image using KLT Optflow Algrithm
+      * @param timestamp - timestamp of image
+      * @param img - image data 
+      */  
     std::vector<cv::Point2f> optiTrackFeature(double timestamp,const cv::Mat& img);
     
 };
