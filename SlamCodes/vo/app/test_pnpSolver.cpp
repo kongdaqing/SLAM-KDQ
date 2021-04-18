014c5c3a4354ec18a0a32791d2f7b79acd9221b4
kongdaqing <kongdaqing@zerozero.cn>
2021-04-18 23:37:05 +0800

    add test module of vizscene and pnpSolver

--- SlamCodes/vo/app/test_pnpSolver.cpp
+++ SlamCodes/vo/app/test_pnpSolver.cpp
@@ -1,6 +1,14 @@
-#include "Camera.hpp"
-using namespace std;
-int main() {
-  if ()
+#include "Simulator.hpp"
+using namespace ov;
+int main(int argc,char **argv) {
+  if (argc < 2) {
+    std::cerr << "Please input config file!" << std::endl;
+    return -1;
+  }
+  Config *cfg = new Config(argv[1]);
+  Camera *cam = new Camera(cfg);
+  Simulator sim(cfg,cam);
+  sim.createLandMarkers();
+  
 
 }
