aa548482b00d9cc75d2e04abb9ab7a079738e8d5
kongdaqing <kongdaqing@zerozero.cn>
2021-05-04 21:43:19 +0800

    [PnpSolver]:add ratio for success judgement and verbose code

--- SlamCodes/vo/include/vo/PnpSolver.hpp
+++ SlamCodes/vo/include/vo/PnpSolver.hpp
@@ -9,20 +9,32 @@ class PnpSolver {
    * @param normalizedUV  ---  normalized features vector  
    * @param matchedPts3D  ---  matched features' 3D vector
    * @param focalLength   ---  camera focal length
-   * @param R             ---  Rotation Matrix 
-   * @param t             ---  translate vec 
+   * @param R             ---  Rotation Matrix from world to camera coordinate
+   * @param t             ---  translate vec from world to camera coordinate
    * @param inlier        ---  inlier vector of reproject 
+   * @param ratio         ---  rotio of inlier/total
    * @return return success otherwise false
    */ 
   bool solveByPnp(const std::vector<cv::Point2f>& normalizedUV,
                   const std::vector<cv::Point3f>& matchedPts3D,
                   double focalLength,cv::Mat &R,cv::Mat &t,
-                  std::vector<uchar> &inlier) {
+                  std::vector<int> &inlier,double ratio) {
     if (normalizedUV.size() < 4) {
       return false;
     }
     cv::Mat K = cv::Mat::eye(3,3,CV_64F);
-    return cv::solvePnPRansac(matchedPts3D,normalizedUV,K,cv::Mat(),R,t,false,50,8.0/focalLength,0.99,inlier);
+    std::cout << focalLength << std::endl;
+    bool resultFlg = cv::solvePnPRansac(matchedPts3D,normalizedUV,K,cv::Mat(),R,t,false,100,8.0/focalLength,0.9,inlier);
+    
+#ifdef VERBOOSE    
+    std::cout << "result flg = " << resultFlg << ",Inlier" << inlier.size() << ":\n" ;
+    for (int i = 0; i < matchedPts3D.size(); i++) {
+      cv::Affine3d T(R,t);
+      cv::Point3f ptInCam =  T * matchedPts3D[i];
+      std::cout << ptInCam/ptInCam.z << " vs " << normalizedUV[i] << std::endl;
+    }
+#endif
+    return resultFlg && inlier.size() > ratio * normalizedUV.size();
   }
 
 };
