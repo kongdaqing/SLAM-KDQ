cf6fdd2ee921fca79c5c331cb1ea13989df63429
kongdaqing <kongdaqing@zerozero.cn>
2021-05-20 07:54:53 +0800

    [d435i]:add config

--- SlamCodes/d435i_sdk/app/test_infrared.cpp
+++ SlamCodes/d435i_sdk/app/test_infrared.cpp
@@ -1,7 +1,11 @@
 #include "d435i.hpp"
 
 int main(int argc,char **argv) {
-  D435I d435i;
+  if (argc < 2) {
+    std::cout << "please input config file!" << std::endl;
+    return -1;
+  }
+  D435I d435i(argv[1]);
   d435i.start();
   while(1) {
     double timestamp;
@@ -9,13 +13,13 @@ int main(int argc,char **argv) {
     if (!d435i.getInfraredImages(timestamp,leftImg,rightImg)) {
       continue;
     }
-    if (!leftImg.empty() && !rightImg.empty()) {
-      cv::Mat img;
-      cv::hconcat(leftImg,rightImg,img);
-      cv::imshow("infraredImages",img);
-      cv::waitKey(1);
-    }
-    cv::waitKey(1);
+//    if (!leftImg.empty() && !rightImg.empty()) {
+//      cv::Mat img;
+//      cv::hconcat(leftImg,rightImg,img);
+//      cv::imshow("infraredImages",img);
+//      cv::waitKey(1);
+//    }
+//    cv::waitKey(1);
   }
   return 0;
 }
--- SlamCodes/d435i_sdk/include/d435i.hpp
+++ SlamCodes/d435i_sdk/include/d435i.hpp
@@ -2,21 +2,57 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
+#include <librealsense2/rs_advanced_mode.hpp>
+struct D435IConfigParam {
+  D435IConfigParam() {
+    printf("[D435I-DefaultParameters]:default values are set according to realsense-viwer!\n");
+    streamType = infrared;
+    width = 640;
+    height = 480;
+    framerate = 30;
+    auto_exposure = 1;
+    exposure_time = 5000.0;
+    exposure_gain = 80.;
+    ae_point = 2000;
+  }
+
+  void print() {
+    std::cout << "streamType = " << streamType << std::endl;
+    std::cout << "width = " << width << std::endl;
+    std::cout << "height = " << height << std::endl;
+    std::cout << "framerate = " << framerate << std::endl;
+    std::cout << "auto_exposure = " << auto_exposure << std::endl;
+    std::cout << "exposure_time = " << exposure_time << std::endl;
+    std::cout << "exposure_gain = " << exposure_gain << std::endl;
+    std::cout << "ae_point = " << ae_point << std::endl;
+  }
+  enum Stream {
+    infrared,
+//    rgb,
+//    depth,
+  }; //当前仅支持双目
+  int streamType;
+  int width;
+  int height;
+  int framerate;
+  int auto_exposure;
+  float exposure_time; //us
+  float exposure_gain;
+  float ae_point; //this value work when auto-exposure is enabled,present mean intensity of image
+};
 
 class D435I {
  public:  
-  D435I() {
-    cfg_.disable_stream(RS2_STREAM_DEPTH);
-    cfg_.enable_stream(RS2_STREAM_INFRARED, 1, 640, 480, RS2_FORMAT_Y8, 30);
-    cfg_.enable_stream(RS2_STREAM_INFRARED, 2, 640, 480, RS2_FORMAT_Y8, 30);
-  }
+  D435I(){};
+
+  D435I(const std::string &cfgFile);
 
   void start();
 
 
-  bool getInfraredImages(double& timestamp,cv::Mat& leftImg,cv::Mat& rightImg);
+  bool getInfraredImages(double& timestamp,cv::Mat& leftImg,cv::Mat& rightImg) const;
 
  private:
   rs2::pipeline pipe_;
-  rs2::config cfg_;
+  D435IConfigParam stereoConfigParam_;
 };
