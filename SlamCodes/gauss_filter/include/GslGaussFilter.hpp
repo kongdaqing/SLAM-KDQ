79213a5e34729cf9b0683a9e73f2987823555c50
kongdaqing <kongdaqing@zerozero.cn>
2021-06-23 17:39:55 +0800

    [gauss-filter]:update filter

--- SlamCodes/gauss_filter/include/GslGaussFilter.hpp
+++ SlamCodes/gauss_filter/include/GslGaussFilter.hpp
@@ -7,6 +7,16 @@
 #include <gsl/gsl_filter.h>
 #include <gsl/gsl_vector.h>
 #include "iostream"
+
+struct GaussFilterParam {
+ public:
+  GaussFilterParam(size_t k,double alp) {
+    K = k;
+    alpha = alp;
+  }
+  size_t K;
+  double alpha;
+};
 class GslGaussFilter {
  public:
   /** \brief set gauss filter through gsl library
@@ -16,9 +26,19 @@ class GslGaussFilter {
    */
   GslGaussFilter(size_t windowSize,double alpha):K_(windowSize),alpha_(alpha) {
     f_ = gsl_filter_gaussian_alloc(K_);
-
   }
 
+  /** \brief set gauss filter paramter
+   * @param para -- gsl gauss filter
+   */
+  GslGaussFilter(GaussFilterParam para):K_(para.K),alpha_(para.alpha) {
+    f_ = gsl_filter_gaussian_alloc(K_);
+  }
+
+  /** \brief apply gauss filter for single dimension
+   * @param rawData --- raw data vector
+   * @return filtered data by gauss filter
+   */
   std::vector<double> apply(std::vector<double>& rawData) {
     gsl_vector *x = gsl_vector_alloc(rawData.size());
     gsl_vector *y = gsl_vector_alloc(rawData.size());
@@ -35,12 +55,16 @@ class GslGaussFilter {
     return outData;
   }
 
-
-  std::vector<Eigen::VectorXd> apply(std::vector<Eigen::VectorXd>& rawData) {
+  /** \brief apply gauss filter for D-th dimension
+   * @tparam D --- dimension of input data
+   * @param rawData --- D-th raw datas
+   * @return filtered data by gauss filter
+   */
+  template<int D>
+  std::vector<Eigen::Matrix<double,D,1>> apply(std::vector<Eigen::Matrix<double,D,1>>& rawData) {
     if (rawData.empty()) {
       return rawData;
     }
-    const size_t D = rawData.front().rows();
     const size_t E = rawData.size();
     std::vector<std::vector<double>> yy;
     for (size_t j = 0; j < D; j++) {
@@ -51,7 +75,7 @@ class GslGaussFilter {
       y = apply(x);
       yy.push_back(y);
     }
-    std::vector<Eigen::VectorXd> outData;
+    std::vector<Eigen::Matrix<double,D,1>> outData;
     Eigen::VectorXd out;
     out.resize(D);
     for (size_t i = 0; i < E; i++) {
