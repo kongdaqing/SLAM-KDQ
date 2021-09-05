4d108a2da1dea76c75709ff1fd4e6b5ad1a631ed
kongdaqing <1533415649@qq.com>
2021-09-05 20:06:12 +0800

    [feature-track]:add remap test

--- SlamCodes/feature_track/src/falcon_test.cpp
+++ SlamCodes/feature_track/src/falcon_test.cpp
@@ -5,6 +5,20 @@
 #include "optiflow_track/vins_optitrack.hpp"
 #include <opencv2/opencv.hpp>
 using namespace std;
+
+void test_remap(cv::Mat& mapx,cv::Mat& mapy) {
+  mapx.create(240,320,CV_32FC1);
+  mapy.create(240,320,CV_32FC1);
+  for (size_t x = 0; x < 320;x++) 
+    for (size_t y = 0;y < 240;y++) {
+      if (y > 40 && y < 200)
+        mapy.at<float>(y,x) = y;
+      else 
+        mapy.at<float>(y,x) = 240 - y;
+      mapx.at<float>(y,x) = x;  
+    }
+}
+
 typedef ::zz::replaykit::ReplayKit<
     ::zz::replaykit::Topics<vision::BottomImage, rovio::InputInfoPack>,
     ::zz::replaykit::Commands<>>
@@ -20,12 +34,15 @@ int main(int argc,char** argv) {
   float replay_speed = argc < 5 ? 1 : atof(argv[4]);
   ReplayKitType replaykit;
   OptiflowTrackByVINS vinsFeatTracker(30,0.1,30,imageStorePath,1);
-
+  // cv::Mat mapx,mapy;
+  // test_remap(mapx,mapy);
   replaykit.Subscribe<0>([&](double now_time, const vision::BottomImage &bottomImage) {
     const cv::Mat im = cv::Mat(cv::Size(bottomImage.width(), bottomImage.height()), CV_8UC1,
                                (char *) bottomImage.image_buffer().c_str()).clone();
     auto delay = static_cast<float>(now_time - bottomImage.timestamp());
     auto exp_time = bottomImage.exposure_time();
+    // cv::Mat img;
+    // cv::remap(im,img,mapx,mapy,cv::INTER_LINEAR);
     if (1) {
       printf("[Image] Get %12.6f at %12.6f, exp = %7.3fms, delay = %7.3fms\n",
              bottomImage.timestamp(), now_time, exp_time * 1e3f, delay * 1e3f);
